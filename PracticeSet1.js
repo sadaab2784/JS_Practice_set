dbd2ee781798c37456d654ed1d759d151314773e
sadaab2784 <ali.sadaab825313@gmail.com>
2022-11-21 00:26:21 +0530

    variable in javascript

--- PracticeSet1.js
+++ PracticeSet1.js
@@ -1,4 +1,5 @@
 console.log("Hello world");
+// output: Hello world
 
 // this program add three number (1,2 and 24)
 
@@ -7,3 +8,16 @@ function addThreeNumbers(a, b, c){
 }
 let sum = addThreeNumbers(1, 2, 24);
 console.log(sum);
+// output: 27
+
+
+// variable in javascript
+let a = 24; // a contains 24 
+console.log(a);
+a = "alone"
+console.log(a);  
+// output: 24 alone
+// note: Javascrit allow us to change a variable in run time
+// let 2alone = 7; // Not allowed this will throw an error
+// let var = 7; // javascript reserved words cannot be used as a variable name
+// note: Alone & alone are two different variable in javascript (case sensitive)
