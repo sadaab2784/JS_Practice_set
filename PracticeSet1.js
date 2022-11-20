e85720a4b66b64524e002271df63e6b5024817e3
sadaab2784 <ali.sadaab825313@gmail.com>
2022-11-20 22:25:31 +0530

    Intro of JS and simple ex

---
+++ PracticeSet1.js
@@ -0,0 +1,9 @@
+console.log("Hello world");
+
+// this program add three number (1,2 and 24)
+
+function addThreeNumbers(a, b, c){
+    return a + b + c;
+}
+let sum = addThreeNumbers(1, 2, 24);
+console.log(sum);
