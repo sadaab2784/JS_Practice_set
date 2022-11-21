85d2365bf3f985c76e96ce61e02204c3fdc918e9
sadaab2784 <ali.sadaab825313@gmail.com>
2022-11-22 00:55:30 +0530

    Javascript Operators and Expressions

---
+++ Prac_set_5.js
@@ -0,0 +1,85 @@
+// PracticeSet-5
+// javascript Operators and Expressions
+
+// Arithmatic Operator
+let a = 10;
+let b =  4;
+console.log("a + b =", a+b);
+// output: a + b = 14
+console.log("a - b =", a-b);
+// output: a - b = 6
+console.log("a / b =", a/b);
+// output: a / b = 2.5
+console.log("a * b =", a*b);
+// output: a * b = 240
+console.log("a ** b =", a**b); // note: a**b , it means a to the power b
+// output: a ** b = 10000
+console.log("a % b =", a%b); // note : a%b, it means its shows the remainder
+// output: a % b = 2
+
+// Increment operator and Decrement operator
+console.log("a++ =", a++); // note : a++ means first print the value of a and then increment the value of a ..
+// output: a++ = 10
+console.log("a =", a);
+// output: a = 11
+console.log("++a =", ++a); // note: ++a means first increment the value of a and then print the value of a ..
+// output: 12
+
+// Example---
+let c = 10;
+console.log("++c =", ++c); // val c = 11
+// output: ++c = 11
+console.log("c++ =", c++); // val c = 12
+// output: c++ = 11
+console.log("--c =", --c); // val c = 11
+// output: --c = 11
+console.log("c-- =", c--); // val c = 10
+// output: c-- = 11
+console.log("c =", c);
+// output: c = 10
+console.log("c-- =", c--); 
+// output: c-- = 10
+
+// Assignment Operator
+a = 8;
+console.log(a);
+// output: 8 
+let assignment = 1;
+assignment += 5; // same as assignment = assignment + 5
+console.log(assignment);
+
+// Comparison Operator
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1 == comp2 is", comp1 == comp2);
+// output: comp1 == comp2 is true
+console.log("comp1 != comp2 is", comp1 != comp2);
+// output: comp1 != comp2 is false
+console.log("comp1 === comp2 is", comp1 === comp2); // note: "===" means that both type and value are same ..
+// output: comp1 === comp2 is false
+console.log("comp1 !== comp2 is", comp1 !== comp2);
+// output: comp1 !== comp2 is ture
+
+// Logical Operator
+let x = 5;
+let y = 6;
+console.log(x<y && x==5);
+// output: true
+console.log(x<y || x==5);
+// output: true
+console.log(x>y || x==5);
+// output: true
+console.log(!false); // output: true
+console.log(!true); // output: false
+
+
+
+
+
+
+
+
+
+
+
+
